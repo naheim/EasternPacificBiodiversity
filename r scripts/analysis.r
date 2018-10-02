2e7f93a7ac997ad40513644ea8ebefe436a3920d
naheim <naheim@gmail.com>
2018-10-02 10:30:41 -0700

    figuring out gis data manipulation

--- r scripts/analysis.r
+++ r scripts/analysis.r
@@ -12,6 +12,9 @@ setwd(paste(my.root,"/Box Sync/git/snapshotcalcoast", sep=""));
 
 library(vegan)
 library(maps)
+require(rgdal)
+
+crswgs84 <- CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs") # map projection
 
 load("data files/calCoastObs.RData")
 
@@ -30,3 +33,15 @@ for(i in 1:3) {
 	points(calCoast$lng[calCoast$year==sampleYears[i]], calCoast$lat[calCoast$year==sampleYears[i]], cex=0.5, col='red')
 }
 
+## maps
+counties <- readOGR(dsn = "data files/CA_Counties/", layer = "CA_Counties_TIGER2016")
+counties <- spTransform(counties, crswgs84)
+
+# subset individual counties
+sanFrancisco <- counties[counties$NAME == 'San Francisco',]
+
+obsCoords <- data.frame('lng'=as.numeric(calCoast$lng), 'lat'=as.numeric(calCoast$lat))
+# Assignment modified according
+coordinates(obsCoords) <- ~ lng + lat
+# Set the projection of the SpatialPointsDataFrame using the projection of the shapefile
+proj4string(obsCoords) <- proj4string(counties)
